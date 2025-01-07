99db8b6fb60899ba0877615d8cb618ede37e34c5
Joao pedro <joao04pedro2006@gmail.com>
2025-01-07 01:02:45 -0300

    update tela signin

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,6 +11,8 @@ import GoTopButton from "./scripts/goTop";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Importa os estilos do carrossel
 import { FaCheck } from "react-icons/fa";
+
+
 export default function Home() {
 
   return (
@@ -30,7 +32,7 @@ export default function Home() {
 
         {/* Links e Botões */}
         <div className="flex gap-4 items-center">
-          <Link href="/" className="text-sm md:text-base">
+          <Link href="#signin-aside" className="text-sm md:text-base">
             Sobre nós
           </Link>
           <Link href="/" className="text-sm md:text-base">
@@ -45,7 +47,7 @@ export default function Home() {
         </div>
       </header>
 
-      <nav className="nav-pag-inital w-full md:h-[60vh] h-[45vh] bg-slate-200 text-center md:py-32 py-20 rounded-b-2xl">
+      <nav className="nav-pag-inital w-full h-[70vh] bg-slate-200 text-center md:py-32 py-20 rounded-b-2xl">
         <h1 className="md:text-4xl text-3xl font-bold">Simplifique Seus Estudos</h1>
         <p className="text-muted-foreground text-center text-sm mt-2 m-2 nowrap">
           Deixe que nós fazemos a curadoria para você. Assine nossa plataforma e receba todos
@@ -57,7 +59,7 @@ export default function Home() {
 
         <div className="w-full h-10 mt-10 px-10">
           <div className="flex w-full max-w-xl items-center space-x-2 m-auto">
-            <Input type="email" placeholder="Email" />
+            <Input type="email" placeholder="exemple@email.com" />
             <Button type="submit">Assine agora</Button>
           </div>
           <p className="text-muted-foreground mt-4 text-xs underline">
@@ -67,14 +69,16 @@ export default function Home() {
       </nav>
 
 
-      <h3 className="text-center mt-14 md:text-4xl text-3xl font-bold">Como funciona?</h3>
 
-      <aside className="w-full h-[80vh] flex justify-center items-center mt-[-4%] p-6 md:p-10 signin-aside">
 
-        <div className="w-full max-w-screen-xl flex flex-col md:flex-row justify-between p-4 md:p-12">
+      <aside id="signin-aside" className="w-full h-auto flex flex-col items-center p-6 md:p-10 mt-4">
+        <h3 className="text-center text-3xl md:text-4xl font-bold">
+          Como funciona?
+        </h3>
 
+        <div className="w-full max-w-screen-xl flex flex-col md:flex-row justify-between items-start gap-8 p-4 md:p-12 mt-0 md:mt-[-4%]">
           {/* Carrossel de imagens */}
-          <div className="w-full md:w-1/2 h-1/2 md:h-full flex justify-center items-center mb-8 md:mb-0">
+          <div className="w-full md:w-1/2 flex justify-center items-center lg:mt-0 mt-20 max-md:mt-[-2%]">
             <Carousel
               autoPlay={true}
               infiniteLoop={true}
@@ -82,7 +86,7 @@ export default function Home() {
               showStatus={false}
               showThumbs={false}
               dynamicHeight={true}
-              className="w-full max-w-4xl m-auto"
+              className="w-full max-w-4xl"
             >
               <div className="flex aspect-square bg-background rounded-lg p-8">
                 <Image src={ImgSite} alt="Logo Site" width={700} height={400} />
@@ -94,33 +98,27 @@ export default function Home() {
           </div>
 
           {/* Texto explicativo */}
-          <div className="w-full md:w-1/2 h-1/2 md:h-full py-8 px-6 md:ml-12">
-            <h1 className="text-center mt-10 text-xl md:text-2xl font-bold text-black">
+          <div className="w-full md:w-1/2 flex flex-col space-y-6 md:mt-28 ">
+            <h1 className="text-center text-xl md:text-2xl font-bold text-black">
               Aqui vai uma breve explicação
             </h1>
-
-            <div className="flex flex-col space-y-6 mt-6 md:px-12 px-6">
-              <div className="flex items-center space-x-3 mt-8 text-center">
+            <div className="flex flex-col space-y-6 mt-4 px-4 md:px-6">
+              <div className="flex items-center space-x-3">
                 <span className="text-black text-lg">Assim</span>
                 <FaCheck className="text-colorPadrao text-2xl" />
               </div>
-              <div className="flex items-center space-x-3 mt-4">
+              <div className="flex items-center space-x-3">
                 <span className="text-black text-lg">Assim</span>
                 <FaCheck className="text-colorPadrao text-2xl" />
               </div>
-              <div className="flex items-center space-x-3 mt-4">
+              <div className="flex items-center space-x-3">
                 <span className="text-black text-lg">Assim</span>
                 <FaCheck className="text-colorPadrao text-2xl" />
               </div>
             </div>
           </div>
-
         </div>
       </aside>
-
-
-
-
     </>
   );
 }
